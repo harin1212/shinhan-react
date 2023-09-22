5362f93ae6a121d7a2a12855e6b2a640679ef86a
harin <harinkim@gmail.com>
2023-09-22 13:04:10 +0900

    해외주식 추가, 리액트에 API 연동

--- src/App.js
+++ src/App.js
@@ -3,21 +3,19 @@ import "./styles/global.css";
 import Home from "./pages/Home/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStock from "./pages/GlobalStock/GlobalStock";
-import TopNav from "./components/TopNav/TopNav";
 
 function App() {
-  return <div className="layout">
-    {/* <TopNav/>
-    <Home /> */}
-    <TopNav/>
-    <BrowserRouter>
+  return (
+    <div className="layout">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/global" element={<GlobalStock />} />
           <Route path="*" element={<div>페이지를 찾을 수 없습니다</div>} />
         </Routes>
       </BrowserRouter>
-  </div>;
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,13 +8,16 @@ import EventLogo2 from "../../assets/images/event_logo_2.png";
 import EventLogo3 from "../../assets/images/event_logo_3.png";
 import EventInfo from "../../components/EventInfo/EventInfo";
 import AccordianListItem from "../../components/AccordianListItem/AccordianListItem";
+import TopNav from "../../components/TopNav/TopNav";
+import { useNavigate } from "react-router-dom";
 
-const events = [
+const eventInformations = [
   { num: 1, title: "기간", text: "2023.07.01(토) ~ 2023.12.31(일)" },
   { num: 3, title: "대상", text: "신한투자증권 생애 첫 계좌 개설 신규 고객" },
 ];
 
 const Home = () => {
+  const navigate = useNavigate();
   const [openAccordion1, setOpenAccordion1] = useState(false);
   const [openAccordion2, setOpenAccordion2] = useState(false);
 
@@ -26,49 +29,55 @@ const Home = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <section className={styles.header}>
-        <div className={styles.headerSubTitle}>지금 계좌 개설하면</div>
-        <div className={styles.headerTitle}>
-          <span className={styles.primaryColor}>수수료 평생혜택</span> 제공
-        </div>
-        {/* 카드 섹션 */}
-        <div className={styles.cardContainer}>
-          <div className={styles.cardImageContainer}>
-            <img
-              className={styles.cardImage}
-              src={HeaderCard}
-              alt="온라인 수수료 평생 무료 혜택"
-            />
+    <>
+      <TopNav />
+      <div className={styles.tabMenuContainer}>
+        <button onClick={() => navigate("/")}>국내 주식</button>
+        <button onClick={() => navigate("/global")}>해외 주식</button>
+      </div>
+      <div className={styles.container}>
+        <section className={styles.header}>
+          <div className={styles.headerSubTitle}>지금 계좌 개설하면</div>
+          <div className={styles.headerTitle}>
+            <span className={styles.primaryColor}>수수료 평생혜택</span> 제공
           </div>
-          <div className={styles.cardTitle}>
-            온라인 국내주식 수수료
-            <br />
-            <span className={styles.primaryColor}>평생 혜택 </span>제공
+          {/* 카드 섹션 */}
+          <div className={styles.cardContainer}>
+            <div className={styles.cardImageContainer}>
+              <img
+                className={styles.cardImage}
+                src={HeaderCard}
+                alt="온라인 수수료 평생 무료 혜택"
+              />
+            </div>
+            <div className={styles.cardTitle}>
+              온라인 국내주식 수수료
+              <br />
+              <span className={styles.primaryColor}>평생 혜택 </span>제공
+            </div>
+            {/* 설명 */}
+            <ul className={styles.cardDesc}>
+              <li>* 유관기관 제비용만 고객 부담(07.01 기준)</li>
+              <li>- 코스피, 코스닥, 코넥스 : 0.00363960%</li>
+              <li>- K-OTC : 0.09091870%</li>
+              <li>- ETF, ETN, ELW : 0.00420870%</li>
+              <li>* 온라인 채널 거래에 한함(증권플러스 제외)</li>
+            </ul>
           </div>
-          {/* 설명 */}
-          <ul className={styles.cardDesc}>
-            <li>* 유관기관 제비용만 고객 부담(07.01 기준)</li>
-            <li>- 코스피, 코스닥, 코넥스 : 0.00363960%</li>
-            <li>- K-OTC : 0.09091870%</li>
-            <li>- ETF, ETN, ELW : 0.00420870%</li>
-            <li>* 온라인 채널 거래에 한함(증권플러스 제외)</li>
-          </ul>
-        </div>
 
-        {/* 기간/대상 */}
-        <div className={styles.infoContainer}>
-          {events.map((event) => (
-            <EventInfo title={event.title} text={event.text} />
-          ))}
-        </div>
-        {/* 혜택받으러가기 버튼 */}
-        <a>
-          <div className={styles.applyButton}>수수료 평생혜택 받으러가기</div>
-        </a>
-      </section>
-      {/* 아코디언 메뉴  */}
-      <section className={styles.linkSection}>
+          {/* 기간/대상 */}
+          <div className={styles.infoContainer}>
+            {eventInformations.map((event) => (
+              <EventInfo title={event.title} text={event.text} />
+            ))}
+          </div>
+          {/* 혜택받으러가기 버튼 */}
+          <a>
+            <div className={styles.applyButton}>수수료 평생혜택 받으러가기</div>
+          </a>
+        </section>
+{/* 아코디언 메뉴  */}
+<section className={styles.linkSection}>
         {/* 메뉴 1 */}
         <div>
           <AccordianListItem
@@ -116,31 +125,33 @@ const Home = () => {
         </div>
       </section>
 
-      {/* 이벤트 유의사항 */}
-      <section className={styles.noticeSction}>
-        <h4 className={styles.noticeTitle}>※ 이벤트 유의사항</h4>
-        <ul className={styles.noticeText}>
-          <li>
-            기간 내 대상 고객이 이벤트를 신청해야 혜택이 적용되며, 신청 시점
-            이후 체결된 거래에 한해 수수료 혜택이 적용됩니다.
-          </li>
-          <li>
-            본 이벤트 생애 첫 계좌 개설 신규 고객 조건은 2023.07.01(토) 이전에
-            당사에서 개설한 계좌가 없는 고객(IRP, DB, DC 계좌 제외) 입니다.
-          </li>
-          <li>
-            이벤트 대상 계좌는 ① 비대면 증권종합계좌(신한알파, 모바일 홈페이지,
-            신한플러스에서 개설한 계좌 한정), ②은행제휴 S-Lite 한정입니다.
-            파운트 등 핀테크 제휴계좌는 제외됩니다.
-          </li>
-        </ul>
-        <footer className={styles.footer}>
-          서울특별시 영등포구 여의대로 70 (신한투자증권 타워) <br />
-          대표이사 김상태 |사업자등록번호 116-81-36684 <br />
-          ©2023 SHINHAN SECURITIES CO.,LTD.
-        </footer>
-      </section>
-    </div>
+
+        {/* 이벤트 유의사항 */}
+        <section className={styles.noticeSction}>
+          <h4 className={styles.noticeTitle}>※ 이벤트 유의사항</h4>
+          <ul className={styles.noticeText}>
+            <li>
+              기간 내 대상 고객이 이벤트를 신청해야 혜택이 적용되며, 신청 시점
+              이후 체결된 거래에 한해 수수료 혜택이 적용됩니다.
+            </li>
+            <li>
+              본 이벤트 생애 첫 계좌 개설 신규 고객 조건은 2023.07.01(토) 이전에
+              당사에서 개설한 계좌가 없는 고객(IRP, DB, DC 계좌 제외) 입니다.
+            </li>
+            <li>
+              이벤트 대상 계좌는 ① 비대면 증권종합계좌(신한알파, 모바일
+              홈페이지, 신한플러스에서 개설한 계좌 한정), ②은행제휴 S-Lite
+              한정입니다. 파운트 등 핀테크 제휴계좌는 제외됩니다.
+            </li>
+          </ul>
+          <footer className={styles.footer}>
+            서울특별시 영등포구 여의대로 70 (신한투자증권 타워) <br />
+            대표이사 김상태 |사업자등록번호 116-81-36684 <br />
+            ©2023 SHINHAN SECURITIES CO.,LTD.
+          </footer>
+        </section>
+      </div>
+    </>
   );
 };
 
